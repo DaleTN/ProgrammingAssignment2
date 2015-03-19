7819f2357daef34213f39cb95904ab2cc9b1ff26
DaleTN <dg.armor@gmail.com>
2015-03-18 19:27:25 -0500

    Final version for marking
    This is my final version for marking for the course assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
+##    cachematrix.R  03/18/2015  Dale G. Armor
+##    There are 2 functions included in this file:
+##
+##    1)  makeCacheMatrix:    This function creates a special "matrix" object that can cache its inverse.
+##    2)  cacheSolve:         This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+##                            If the inverse has already been calculated (and the matrix has not changed), then the
+##                            cacheSolve function should retrieve the inverse from the cache.
+
 makeCacheMatrix <- function(x = matrix()) {
       m <- NULL
       ## create the variable "m"
       evn <- environment()
       ## Save the current function's environment value
 
+
+      ## Although the assignment said to assume all matricies could have an inverse created,
+      ## that is not true.  By using the following if statement it checks if the matrix can have
+      ## a valid inverse calculated.  This is done to prevent any makeCacheMatrix function failures.
+
       if (det(x)!=0) {
 
             set <- function(y) {
-                  ## save the value of Y with name "x" in the parent environment
+                  ## save the value of y with label "x" and initialize "m" in the parent environment
                   x <<- y
-                  ## create "m" with value "NULL" in the parent environment
                   m <<- NULL
             }
             get <- function() x                       ## function to recover matrix data for recalculation from the cache
@@ -23,30 +35,28 @@ makeCacheMatrix <- function(x = matrix()) {
                  setinv = setinv,
                  getinv = getinv,
                  getevn = getevn)
+
       } else {
 
-            message("This matrix is not valid for inverse calculation") ## identifies matrix submitted not valid for this function
+            ## identifies matrix data submitted not valid for this function
+            message("This matrix is not valid for inverse calculation")
       }
 }
 
-
-## Write a short comment describing this function
+##    Second Function
 
 ## This function will either calculate the inverse of a matrix or will recover the previously calculated value from Cache if available
 
 cacheSolve <- function(x, ...) {
 
-      ## Return a matrix that is the inverse of 'x'
+      m <- x$getinv()                           ## Gets the cached inverted matrix if available
 
-      m <- x$getinv()
-      ## Return a matrix that is the inverse of the original supplied to function "makeCacheMatrix"
-
-      if(!is.null(m)) {                         ## Checking if there is cached data
+      if(!is.null(m)) {                         ## Checking if there is a cached matrix
             message("getting cached data")
-            return(m)                           ## Returning the cached matrix
+            return(m)                           ## outputing the cached matrix
       }
-      data <- x$get()
+      data <- x$get()                           ## Retrieval of matrix data from cache
       m <- solve(data, ...)                     ## Calculating the inverse of the matrix
-      x$setinv(m)                               ## Setting the value in the cache for future use
-      m
+      x$setinv(m)                               ## Setting the value the calculated  matrix in the cache for future re-use
+      m                                         ## Outputing the calculated matrix
 }
